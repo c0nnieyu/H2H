301f61287d4ba961ad0a2dd77895e24d21d2e7d8
Ben Clark <brclark15@gmail.com>
2018-10-13 17:41:49 -0500

    Send categories to index view

--- backend/ohgeez/views.py
+++ backend/ohgeez/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
-#def index(request):
-#  return HttpResponse("Hello, world")
+from .models import Category
 
 class HomePageView(TemplateView):
-  def get(self, request, **kwargs):
-    return render(request, 'index.html', context=None)
+    def get(self, request, **kwargs):
+        categories = Category.objects.order_by('name')
+        context = { 'categories': categories }
+        return render(request, 'index.html', context)
 
 class TestPageView(TemplateView):
   def get(self, request, **kwargs):
