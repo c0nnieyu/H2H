dae6d714bfb923a69929a3bcb621fcf8cf9ff3ec
austin-carnahan <ac6404@gmail.com>
2018-10-13 16:41:27 -0500

    fix duplicate name error

--- backend/ohgeez/models.py
+++ backend/ohgeez/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.core.validators import RegexValidator
 from django.utils.translation import ugettext_lazy as _
 
-from geopy.geocoders import Nominatim
 
 class Category(models.Model):
     # Fields
---
+++ backend/requirements.py
@@ -0,0 +1,3 @@
+Django==2.1.2
+django-contrib-comments==1.9.0
+pytz==2018.5
